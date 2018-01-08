f11892378c05f598c4b194f8a0b36f0e554f8c05
aburgos <aburgosd91@gmail.com>
2018-01-08 11:57:40 -0500

    Datos Cambios v37: * Reporte servicios cruzados

--- NisiraCore/src/com/nisira/core/dao/OrdenservicioclienteDao.java
+++ NisiraCore/src/com/nisira/core/dao/OrdenservicioclienteDao.java
@@ -403,6 +403,149 @@ public class OrdenservicioclienteDao extends BaseDao<Ordenserviciocliente> {
             }
             return lista;
         }
+        public ArrayList<Reporte_facturacion> listar_servicios_cruzados_detalladoFiltroFecha(String idempresa,String fechainicio,String fechafin,String idtiposervicio) throws NisiraORMException,Exception {
+            ArrayList<Reporte_facturacion> lista = new ArrayList<Reporte_facturacion>();
+
+            ResultSet rs = null;
+            rs = execProcedure("RPT_SERVICIOS_CRUZADOS_TMPSS",idempresa,fechainicio,fechafin,idtiposervicio);
+            while (rs.next()) {
+                Reporte_facturacion reporte_facturacion = new Reporte_facturacion();
+                reporte_facturacion.setItem(rs.getInt("ITEM"));
+                reporte_facturacion.setIdclieprov(rs.getString("IDCLIEPROV")!=null?rs.getString("IDCLIEPROV").trim():"");
+                reporte_facturacion.setRazon_social(rs.getString("RAZON_SOCIAL")!=null?rs.getString("RAZON_SOCIAL").trim():"");
+                reporte_facturacion.setIddocumento(rs.getString("IDDOCUMENTO")!=null?rs.getString("IDDOCUMENTO").trim():"");
+                reporte_facturacion.setSerie(rs.getString("SERIE")!=null?rs.getString("SERIE").trim():"");
+                reporte_facturacion.setNumero(rs.getString("NUMERO")!=null?rs.getString("NUMERO").trim():"");
+                reporte_facturacion.setFecha(rs.getDate("FECHA"));
+                reporte_facturacion.setFecha_operacion(rs.getDate("FECHA_OPERACION"));
+                reporte_facturacion.setVencimiento(rs.getDate("VENCIMIENTO"));
+                reporte_facturacion.setIdmoneda(rs.getString("IDMONEDA")!=null?rs.getString("IDMONEDA").trim():"");
+                reporte_facturacion.setIdcuenta(rs.getString("IDCUENTA")!=null?rs.getString("IDCUENTA").trim():"");
+                reporte_facturacion.setIdccosto(rs.getString("IDCCOSTO")!=null?rs.getString("IDCCOSTO").trim():"");
+                reporte_facturacion.setConcepto(rs.getString("CONCEPTO")!=null?rs.getString("CONCEPTO").trim():"");
+                reporte_facturacion.setIdcliente(rs.getString("IDCLIENTE")!=null?rs.getString("IDCLIENTE").trim():"");
+                reporte_facturacion.setIdregimen(rs.getString("IDREGIMEN")!=null?rs.getString("IDREGIMEN").trim():"");
+                reporte_facturacion.setAfecto(rs.getFloat("AFECTO"));
+                reporte_facturacion.setInafecto(rs.getFloat("INAFECTO"));
+                reporte_facturacion.setIdimpuesto(rs.getString("IDIMPUESTO")!=null?rs.getString("IDIMPUESTO").trim():"");
+                reporte_facturacion.setImpuesto(rs.getFloat("IMPUESTO"));
+                reporte_facturacion.setTotal(rs.getFloat("TOTAL"));
+                reporte_facturacion.setOrdenregistro(rs.getInt("ORDENREGISTRO"));
+                reporte_facturacion.setEsdetraccion(rs.getFloat("ESDETRACCION"));
+                reporte_facturacion.setTipodetraccion(rs.getString("TIPODETRACCION")!=null?rs.getString("TIPODETRACCION").trim():"");
+                reporte_facturacion.setTasadetraccion(rs.getFloat("TASADETRACCION"));
+                reporte_facturacion.setTcosto(rs.getFloat("TCOSTO"));
+                
+                reporte_facturacion.setDidordenservicio(rs.getString("DIDORDENSERVICIO")!=null?rs.getString("DIDORDENSERVICIO").trim():"");
+                reporte_facturacion.setDcliente(rs.getString("DCLIENTE")!=null?rs.getString("DCLIENTE").trim():"");
+                reporte_facturacion.setDiddocumento(rs.getString("DIDDOCUMENTO")!=null?rs.getString("DIDDOCUMENTO").trim():"");
+                reporte_facturacion.setDserie(rs.getString("DSERIE")!=null?rs.getString("DSERIE").trim():"");
+                reporte_facturacion.setDnumero(rs.getString("DNUMERO")!=null?rs.getString("DNUMERO").trim():"");
+                reporte_facturacion.setDambito_servicio(rs.getString("DAMBITO_SERVICIO_DES")!=null?rs.getString("DAMBITO_SERVICIO_DES").trim():"");
+                reporte_facturacion.setAmbito_servicio(rs.getString("DAMBITO_SERVICIO_DES")!=null?rs.getString("DAMBITO_SERVICIO_DES").trim():"");
+                reporte_facturacion.setIdambito_servicio(rs.getString("DAMBITO_SERVICIO")!=null?rs.getString("DAMBITO_SERVICIO").trim():"");
+                reporte_facturacion.setDfecha_osc(rs.getDate("DFECHA_OSC"));
+                reporte_facturacion.setDfecharegistro(rs.getDate("DFECHAREGISTRO"));
+                reporte_facturacion.setDfechafinregistro(rs.getDate("DFECHAFINREGISTRO"));
+                reporte_facturacion.setDni(rs.getString("DNI")!=null?rs.getString("DNI").trim():"");
+                /*HORAS*/
+                if(rs.getObject("DHI")!=null){
+                    reporte_facturacion.setDhi(((BigDecimal)rs.getObject("DHI")).floatValue());
+                    reporte_facturacion.setDhi_s(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhi()));
+                }else{
+                    reporte_facturacion.setDhi(null);
+                    reporte_facturacion.setDhi_s("");
+                }
+                if(rs.getObject("DHF")!=null){
+                    reporte_facturacion.setDhf(((BigDecimal)rs.getObject("DHF")).floatValue());
+                    reporte_facturacion.setDhf_s(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhf()));
+                }else{
+                    reporte_facturacion.setDhf(null);
+                    reporte_facturacion.setDhf_s("");
+                }
+                if(rs.getObject("DHS")!=null){
+                    reporte_facturacion.setDhs(rs.getFloat("DHS"));
+                    reporte_facturacion.setDhs_s(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhs()));
+                }else{
+                    reporte_facturacion.setDhs(null);
+                    reporte_facturacion.setDhs_s("");
+                }
+                if(rs.getObject("DHBASE")!=null){
+                    reporte_facturacion.setDhbase(rs.getFloat("DHBASE"));
+                    reporte_facturacion.setDhbase_s(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhbase()));
+                }else{
+                    reporte_facturacion.setDhbase(null);
+                    reporte_facturacion.setDhbase_s("");
+                }
+                if(rs.getObject("DHADICIONAL")!=null){
+                    reporte_facturacion.setDhadicional(rs.getFloat("DHADICIONAL"));
+                    reporte_facturacion.setDhadicional_s(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhadicional()));
+                }else{
+                    reporte_facturacion.setDhadicional(null);
+                    reporte_facturacion.setDhadicional_s("");
+                }
+                /****** DATO TAREO ****/
+                if(rs.getObject("DHORA_REQ")!=null){
+                    reporte_facturacion.setDhora_req(((BigDecimal)rs.getObject("DHORA_REQ")).floatValue());
+                    reporte_facturacion.setShora_req(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhora_req()));
+                }else{
+                    reporte_facturacion.setDhora_req(null);
+                    reporte_facturacion.setShora_req("");
+                }
+                if(rs.getObject("DHORA_LLEGADA")!=null){
+                    reporte_facturacion.setDhora_llegada(((BigDecimal)rs.getObject("DHORA_LLEGADA")).floatValue());
+                    reporte_facturacion.setShora_llegada(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhora_llegada()));
+                }else{
+                    reporte_facturacion.setDhora_llegada(null);
+                    reporte_facturacion.setShora_llegada("");
+                }
+                if(rs.getObject("DHORA_INICIO_SERV")!=null){
+                    reporte_facturacion.setDhora_inicio_serv(((BigDecimal)rs.getObject("DHORA_INICIO_SERV")).floatValue());
+                    reporte_facturacion.setShora_inicio_serv(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhora_inicio_serv()));
+                }else{
+                    reporte_facturacion.setDhora_inicio_serv(null);
+                    reporte_facturacion.setShora_inicio_serv("");
+                }
+                if(rs.getObject("DHORA_FIN_SERV")!=null){
+                    reporte_facturacion.setDhora_fin_serv(((BigDecimal)rs.getObject("DHORA_FIN_SERV")).floatValue());
+                    reporte_facturacion.setShora_fin_serv(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhora_fin_serv()));
+                }else{
+                    reporte_facturacion.setDhora_fin_serv(null);
+                    reporte_facturacion.setShora_fin_serv("");
+                }
+                if(rs.getObject("DHORA_LIBERACION")!=null){
+                    reporte_facturacion.setDhora_liberacion(((BigDecimal)rs.getObject("DHORA_LIBERACION")).floatValue());
+                    reporte_facturacion.setShora_liberacion(CoreUtil.convertTimeFloatString(reporte_facturacion.getDhora_liberacion()));
+                }else{
+                    reporte_facturacion.setDhora_liberacion(null);
+                    reporte_facturacion.setShora_liberacion("");
+                }
+                reporte_facturacion.setDchecklist(rs.getString("DCHECKLIST")!=null?rs.getString("DCHECKLIST").trim():"");
+                reporte_facturacion.setDidvehiculo(rs.getString("DIDVEHICULO")!=null?rs.getString("DIDVEHICULO").trim():"");
+                reporte_facturacion.setDnrocontenedor(rs.getString("DNROCONTENEDOR")!=null?rs.getString("DNROCONTENEDOR").trim():"");
+                reporte_facturacion.setDnroprecinto(rs.getString("DNROPRECINTO")!=null?rs.getString("DNROPRECINTO").trim():"");
+                reporte_facturacion.setDnro_oservicio(rs.getString("DNRO_OSERVICIO")!=null?rs.getString("DNRO_OSERVICIO").trim():"");
+                reporte_facturacion.setDplaca_cliente(rs.getString("DPLACA_CLIENTE")!=null?rs.getString("DPLACA_CLIENTE").trim():"");
+                reporte_facturacion.setDconductor_cliente(rs.getString("DCONDUCTOR_CLIENTE")!=null?rs.getString("DCONDUCTOR_CLIENTE").trim():"");
+                reporte_facturacion.setDbrevete_cliente(rs.getString("DBREVETE_CLIENTE")!=null?rs.getString("DBREVETE_CLIENTE").trim():"");
+                
+                reporte_facturacion.setDcosto_rh(rs.getFloat("DCOSTO_RH"));
+                reporte_facturacion.setDhcosto_adicional(rs.getFloat("DHCOSTO_ADICIONAL"));
+                reporte_facturacion.setDcosto_bono(rs.getFloat("DCOSTO_BONO"));
+                reporte_facturacion.setDidcargo(rs.getString("DIDCARGO")!=null?rs.getString("DIDCARGO").trim():"");
+                reporte_facturacion.setDcodigo_ec(rs.getString("DCODIGO_EC")!=null?rs.getString("DCODIGO_EC").trim():"");
+                reporte_facturacion.setDitemrango_ec(rs.getString("DITEMRANGO_EC")!=null?rs.getString("DITEMRANGO_EC").trim():"");
+                reporte_facturacion.setDcodoperaciones_ec(rs.getString("DCODOPERACIONES_EC")!=null?rs.getString("DCODOPERACIONES_EC").trim():"");
+                reporte_facturacion.setDnhoras_ec(rs.getFloat("DNHORAS_EC"));
+                reporte_facturacion.setDidruta_ec(rs.getString("DIDRUTA_EC")!=null?rs.getString("DIDRUTA_EC").trim():"");
+                reporte_facturacion.setDidruta(rs.getString("DIDRUTA")!=null?rs.getString("DIDRUTA").trim():"");
+                reporte_facturacion.setCargo(rs.getString("DCARGO")!=null?rs.getString("DCARGO").trim():"");
+                reporte_facturacion.setRutaservicio(rs.getString("RUTASERVICIOS")!=null?rs.getString("RUTASERVICIOS").trim():"");
+                reporte_facturacion.setEsplanilla(rs.getInt("ESPLANILLA"));
+                lista.add(reporte_facturacion); 
+            }
+            return lista;
+        }
         public NSRResultSet getConsultaRepote(String idempresa,String fechainicio,String fechafin,String cliente,String supervisor) throws NisiraORMException{
             ResultSet rs = null;
             rs = execProcedure("RPT_ORDENSERVICIOCLIENTE_TMPSS",idempresa,fechainicio,fechafin,cliente,supervisor);
